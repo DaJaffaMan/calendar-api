50661e6aa0fc979391283f9f6c6a1aacfa7b4ff3
DaJaffaMan <jack.h.jefferies@gmail.com>
2020-04-02 12:37:06 +0100

    fix tests

--- test/controllers/calendar.test.ts
+++ test/controllers/calendar.test.ts
@@ -1,11 +1,11 @@
 import _jest from 'jest';
-import {calendarController} from '../../src/controllers'
+import { calendarControllerV1 } from '../../src/controllers'
 import { Request, Response } from 'express';
 
 test("Should return calendars", async () => {
     const mockRequest = {} as Request
     const mockResponse = {} as Response;
-    const actual = await calendarController.getCalendars(mockRequest, mockResponse, () => {})
+    const actual = await calendarControllerV1.getCalendars(mockRequest, mockResponse, () => {})
 
     expect(actual).toStrictEqual([{name: 'fam'}])
 });
