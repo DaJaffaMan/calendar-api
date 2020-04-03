1150685e7a38feae4dab96b6076448390d83ad35
DaJaffaMan <jack.h.jefferies@gmail.com>
2020-04-03 14:46:30 +0100

    fix test to use mock repository

--- test/controllers/calendar.test.ts
+++ test/controllers/calendar.test.ts
@@ -1,12 +1,18 @@
 import _jest from 'jest';
+import { mocked } from 'ts-jest/utils'
 import { calendarControllerV1 } from '../../src/controllers'
 import { Request, Response } from 'express';
+import { calendarRepository } from '../../src/repositories/calendar';
+
+jest.mock('../../src/repositories/calendar');
 
 test("Should return calendars", async () => {
-    const mockRequest = {} as Request
+    const mockRequest = {} as Request;
     const mockResponse = {} as Response;
-    const actual = await calendarControllerV1.getCalendars(mockRequest, mockResponse, () => {})
-
-    expect(actual).toStrictEqual([{name: 'fam'}])
+    const mockRepoistory = mocked(calendarRepository, true)
+    mockRepoistory.retrieveCalendars.mockReturnValue(Promise.resolve([{id: 1, name: "family"}]));
+    
+    const actual = await calendarControllerV1.getCalendars(mockRequest, mockResponse, () => {});
+    expect(actual).toStrictEqual([{id: 1, name: 'family'}]);
 });
 
